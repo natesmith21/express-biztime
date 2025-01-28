6a327b82e148afac485c9e2159fc7f493a6757a3
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2025-01-28 12:34:36 -0800

    companies routes

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 /** BizTime express application. */
-
-
 const express = require("express");
-
 const app = express();
 const ExpressError = require("./expressError")
 
 app.use(express.json());
 
+const companyRoutes = require('./routes/companies');
+app.use('/companies', companyRoutes);
 
 /** 404 handler */
 
